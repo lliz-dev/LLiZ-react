81c489321b919f1a0a89e33f01361e9f2d06abaf
EthanLeRoux <127761539+EthanLeRoux@users.noreply.github.com>
2026-01-20 00:58:40 +0200

    Import useEffect in BlogMaker for improved state management

--- src/Screens/BlogStuff/BlogMaker.jsx
+++ src/Screens/BlogStuff/BlogMaker.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import { useNavigate } from "react-router-dom"; // <-- import useNavigate
 import Editor from "../../Editor.jsx";
 import postBlog from "../../js/PostBlog.js";
